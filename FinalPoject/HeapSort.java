aff35e41cb891414de9e8b3b40a5557100acc212
Nam <80585478+letCodeGodotgo@users.noreply.github.com>
2021-11-27 14:49:35 +0900

    Update HeapSort.java

--- FinalPoject/HeapSort.java
+++ FinalPoject/HeapSort.java
@@ -1,35 +1,49 @@
 package FinalPoject;
 
-public class HeapSort<T> {
-	public void swap (T[] a, T ptr1, T ptr2) {
-		T t = a[ptr1];
-		a[ptr1] = a[ptr2];
-		a[ptr2] = t;
-	}
+public class HeapSort {
+	public static void sort (int[] a) { sort (a, a.length);}
 	
-	public void downHeap(T[] a, T left, T right) {
-		T temp = a[left];
-		T parent;
-		T child;
+	private static void sort(int[] a, int size) {
+		if(size < 2)
+			return;
 		
-		for (parent = left; parent < (right + 1) / 2; parent = child) {
-			int chl = parent * 2 + 1;
-			int chr = chl + 1;
-			child = (chr <= right && a[chr] > a[chl]) ? chr: chl;
-			if (temp >= a[child])
-				break;
-			a[parent] = a[child];
-		}
-		a[parent] = temp;
-	}
-	
-	public void Heapsort(T[] a, T n) {
-		for (int i = (n - 1) / 2; i >= 0; i--)
-			downHeap(a, i, n - 1);
+		int parentIdx = getParent(size - 1);
 		
-		for (int i = n - 1; i > 0; i--) {
+		for(int i = parentIdx; i >= 0; i--)
+			heap(a, i, size - 1);
+		
+		for (int i = size - 1; i > 0; i--) {
 			swap(a, 0, i);
-			downHeap(a, 0, i - 1);
+			heap(a, 0, i - 1);
+		}
+	}
+	
+	private static int getParent (int child) {
+		return (child - 1) / 2;
+	}
+	
+	private static void swap(int[] a, int i, int j) {
+		int temp = a[i];
+		a[i] = a[j];
+		a[j] = temp;
+	}
+	
+	private static void heap (int[] a, int parentIdx, int lastIdx) {
+		int leChIdx = 2 * parentIdx + 1;	//Left Child index
+		int riChIdx = 2 * parentIdx + 2;	// Right Child index
+		int largIdx = parentIdx;	//Largest index
+		
+		//compare left child node
+		if (leChIdx <= lastIdx && a[largIdx] < a[leChIdx])
+			largIdx = leChIdx;
+		
+		// compare right child node
+		if (riChIdx <= lastIdx && a[largIdx] < a[riChIdx])
+			largIdx = riChIdx;
+		
+		if (parentIdx != largIdx) {
+			swap(a, largIdx, parentIdx);
+			heap(a, largIdx, lastIdx);
 		}
 	}
 
